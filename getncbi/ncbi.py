6a6f3c82f331ccb0e5c0627e46ab2706ecbf019b
qiaohan <qiao_han_ok@163.com>
2016-03-31 08:39:56 +0800

    db add

--- getncbi/ncbi.py
+++ getncbi/ncbi.py
@@ -7,9 +7,19 @@ import re
 class ncbi_spider:
     def __init__(self,root,query):
         self.root_url = root
+        self.query = query 
+        #request = urllib2.Request(data)
+        response = urllib2.urlopen(root)
+        self.listhtml = response.read()
+
+    def html():
+        return listhtml
 
 if __name__=="__main__":
     root_url = "http://www.ncbi.nlm.nih.gov/pubmed/"
-    query = "olaparib erlotinib"
-    html = ncbi_spider.html()
+    #query = "olaparib erlotinib"
+    query = "genome"
+    #root_url = "http://www.baidu.com"
+    spider = ncbi_spider(root_url,query)
+    html = spider.html()
     print html
